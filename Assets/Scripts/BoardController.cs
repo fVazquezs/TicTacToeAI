a9acccd51840883a2230496cdb881926c6223ef3
Fernando <vzs.fernando@gmail.com>
2020-10-25 10:25:16 -0300

    logic of adding cross and circle

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour
+{
+   public void SpotClicked(Spot spot)
+   {
+       Debug.Log($"Line {spot.line} Column {spot.column}");
+
+       switch(spot.CurrentSymbol)
+       {
+           case Symbol.None:
+                spot.CurrentSymbol = Symbol.Cross;
+                break;
+           case Symbol.Cross:
+                spot.CurrentSymbol = Symbol.Circle;
+                break;
+           case Symbol.Circle:
+                spot.CurrentSymbol = Symbol.None;
+                break;
+       }
+   }
+}
---
+++ Assets/Scripts/Spot.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Symbol
+{
+    None,
+    Cross,
+    Circle
+}
+
+public class Spot : MonoBehaviour
+{
+
+    public int line;
+    public int column;
+
+    public GameObject CrossObjectRoot;
+    public GameObject CircleObjectRoot;
+
+    private Symbol _currentSymbol;
+
+    public Symbol CurrentSymbol
+    {
+        set
+        {
+            _currentSymbol = value;
+            CrossObjectRoot.SetActive(_currentSymbol == Symbol.Cross);
+            CircleObjectRoot.SetActive(_currentSymbol == Symbol.Circle);
+
+        }
+        get { return _currentSymbol; }
+    }
+
+    private void Start()
+    {
+        CurrentSymbol = Symbol.None;
+    }
+    private void OnMouseDown()
+    {
+        GetComponentInParent<BoardController>().SpotClicked(this);
+    }
+}
