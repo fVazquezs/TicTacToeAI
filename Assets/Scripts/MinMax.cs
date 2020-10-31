4a5b1a6f33c434a201bae81d7c09dde72d351c9d
Fernando <vzs.fernando@gmail.com>
2020-10-31 12:24:46 -0300

    Adding editor configuration and fixing DoMinMax out issue with dummy code

--- Assets/Scripts/MinMax.cs
+++ Assets/Scripts/MinMax.cs
@@ -8,6 +8,11 @@
 
     public static bool DoMinMax(BoardController board, Symbol player, out Play bestPlay)
     {
+        Play possiblePlay = new Play();
+        possiblePlay.Column = 0;
+        possiblePlay.Line = 0;
+        possiblePlay.Score = 200;
+        bestPlay = possiblePlay;
         return true;
     }
 }
